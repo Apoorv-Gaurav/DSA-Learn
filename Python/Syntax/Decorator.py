c08f340ab00338d70998960b5cab12334d400dce
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2025-10-24 15:52:50 +0000

    Decorator updated

--- Python/Syntax/Decorator.py
+++ Python/Syntax/Decorator.py
@@ -10,3 +10,21 @@ def greet():
     print("Hello!")
 
 greet()
+
+
+import time
+
+def timer(func):
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"Time taken: {end - start:.4f} seconds")
+    return wrapper
+
+@timer
+def slow_function():
+    time.sleep(2)
+    print("Function finished!")
+
+slow_function()
