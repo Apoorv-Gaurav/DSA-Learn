6de6186d90b58183da148c67aeabc55424df036e
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2025-10-25 06:43:01 +0000

    LinlkedlList Updated

--- Python/Syntax/LinkedList.py
+++ Python/Syntax/LinkedList.py
@@ -36,3 +36,56 @@ def display(head):
     print(' --> '.join(elements))
 
 display(head) # 1 --> 3 --> 4 --> 7
+
+#Search for node value - 0(n)
+def search(head, val):
+    curr=head
+    while curr:
+        if val == curr.val:
+            return True
+        curr = curr.next
+    return False
+
+print(search(head,2))
+print(search(head,3))
+
+#-------------------------------------------------------------
+#Doubly Linked List
+# 
+class DoublyNode:
+    def __init__(self,val,next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.val)
+    
+head = tail = DoublyNode(1)
+print(tail)
+
+    #Display - 0(n)
+def display(head):
+    curr=head
+    elements=[]
+    while curr:
+        elements.append(str(curr.val))
+        curr= curr.next
+    print(' <-> '.join(elements))
+display(head)
+
+# Insert at beginning - 0(1)
+def insert_at_beginning(head,tail,val):
+    new_node = DoublyNode(val, next=head)
+    head.prev = new_node
+    return new_node, tail
+head, tail = insert_at_beginning(head, tail, 3)
+display(head)
+
+# Insert at end - 0(1)
+def insert_at_beginning(head,tail,val):
+    new_node = DoublyNode(val, prev=tail)
+    tail.next = new_node
+    return head, new_node
+head, tail = insert_at_beginning(head, tail, 3)
+display(head)
