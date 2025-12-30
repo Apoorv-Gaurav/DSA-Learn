27b76e1820d363e8bebf7186bbf60a5c002a3eb9
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2025-12-30 13:49:49 +0530

    Abstract Class

---
+++ cpp/oops/Class4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Abstract Class
+
+class Shape{
+    virtual void draw() = 0; // pure virtual function
+
+};
+
+class Circle: public Shape{
+public:
+    void draw(){
+        cout<<"Drawing Circle"<<endl;
+    }
+};
+
+int main(){
+    //Shape s1; #Error : cannot create object of abstract class
+    Circle c1;
+    c1.draw();
+    return 0;
+}
