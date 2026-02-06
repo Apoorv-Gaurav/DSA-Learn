07b36da66d663083fa05c257d32bf3942d45b9fc
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-02-07 00:29:43 +0530

    SecondLargest done

--- Practice/Searching/SecondLargest/SecondLargest.c
+++ Practice/Searching/SecondLargest/SecondLargest.c
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <limits.h>
 
-int main(){
-    int arr[] = { 12, 35, 1, 10, 34, 1 };
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int f_largest = arr[0];
-    int s_largest = arr[0];
-    for(int i=1;i<n;i++){
-        if(arr[i]>f_largest){
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int f_largest = INT_MIN;
+    int s_largest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > f_largest) {
             s_largest = f_largest;
             f_largest = arr[i];
         }
-        else if(arr[i]>s_largest){
+        else if (arr[i] > s_largest && arr[i] < f_largest) {
             s_largest = arr[i];
         }
-        
     }
+
     printf("%d", s_largest);
     return 0;
 }
