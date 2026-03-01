5e4d0b9a9223e7241b936eddb1e6e3578d3e773c
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-03-01 11:01:44 +0530

    moved to right place

---
+++ Basics/Tree/treeImplementation.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;//No need
+}
+//Inorder (Left → Root → Right)
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+//Preorder (Root → Left → Right)
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+//Postorder (Left → Right → Root)
+void postorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+
+int main() {
+    struct Node* root = NULL;
+
+    root = insert(root, 10);
+    insert(root, 5);
+    insert(root, 20);
+    insert(root, 3);
+    insert(root, 7);
+    insert(root, 30);
+
+    printf("Inorder traversal: ");
+    inorder(root);
+    printf("\n\n");
+
+    printf("Preorder traversal: ");
+    preorder(root);
+    printf("\n\n");
+
+    printf("Postorder traversal: ");
+    postorder(root);
+    printf("\n\n");
+
+    return 0;
+}
