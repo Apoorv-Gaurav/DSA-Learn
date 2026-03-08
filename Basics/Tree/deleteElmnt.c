0be98d41c4bf5ba6735ad5f90c8f2452fe67c32c
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-03-08 18:34:43 +0530

    deepestElmnt done

---
+++ Basics/Tree/deleteElmnt.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* deepestNode(struct Node* root){
+
+    struct Node* q[100];
+    struct Node *temp = NULL;
+    int front = 0;
+    int rear = -1;
+    q[++rear] = root;
+    while(front<=rear){
+        temp = q[front++];
+        if(temp->left){
+            q[++rear] = temp->left;
+        }
+        if(temp->right){
+            q[++rear] = temp->right;
+        }
+    }
+    return temp;
+}
+
+int main(){
+
+    /*
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+               /
+              7
+    */
+
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    root->right->right = createNode(6);
+    root->right->right->left = createNode(7);
+
+    struct Node* result = deepestNode(root);
+
+    if(result != NULL)
+        printf("Deepest Node: %d\n", result->data);
+
+    return 0;
+}
