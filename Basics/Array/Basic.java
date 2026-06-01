6a4e7c06f8096003bb139c8b75001d542f679f47
Apoorv-Gaurav <apoorvstorm9151@gmail.com>
2026-06-01 07:49:38 +0530

    Leetcode Problem 1 done

---
+++ Basics/Array/Basic.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,41};
+        System.out.println("Hello");
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
---
+++ Basics/String/basic.java
@@ -0,0 +1,11 @@
+
+public class basic {
+
+    public static void main(String args[]) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+
+        // txt.length()
+        
+    }
+}
